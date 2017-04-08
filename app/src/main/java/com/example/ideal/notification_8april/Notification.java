a27d6a2a96b1129b799de3ce8873cec9c518f5f7
pooja6april <verma.pooja0693@gmail.com>
2017-04-08 12:31:08 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ideal/notification_8april/Notification.java
@@ -0,0 +1,16 @@
+package com.example.ideal.notification_8april;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ideal on 4/8/2017.
+ */
+
+public class Notification extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification);
+    }
+}
